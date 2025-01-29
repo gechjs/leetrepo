31ab41dace3604651aff64a745b90de33191c692
gech <gizachew980@gmail.com>
2025-01-29 23:31:54 +0300

    Time: 0 ms (100%), Space: 17.5 MB (72.17%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.py
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.py
@@ -7,6 +7,6 @@ class Solution:
                 return t.upper()
         count = 0
         for i in range(1, len(s)):
-            if s[i] != s[i-1] and s[i] != swapCase(s[i-1]):
+            if s[i] != s[i-1] and s[i] != s[i-1].swapcase():
                 count+=1
         return count
