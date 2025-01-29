fbfbd5139f38800491f3270a697a2a1a23bfb151
gech <gizachew980@gmail.com>
2025-01-29 23:31:11 +0300

    Time: 4 ms (10.92%), Space: 17.6 MB (57.87%) - LeetHub

---
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countKeyChanges(self, s: str) -> int:
+        def swapCase(t):
+            if t == t.upper():
+                return t.lower()
+            else:
+                return t.upper()
+        count = 0
+        for i in range(1, len(s)):
+            if s[i] != s[i-1] and s[i] != swapCase(s[i-1]):
+                count+=1
+        return count
