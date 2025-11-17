7b012e7688e35b692ba8ad1c0262ef633e9cdf25
Gizachew Mohammed <gizachew980@gmail.com>
2025-11-18 01:10:24 +0300

    Time: 2960 ms (27.89%), Space: 18.4 MB (32.56%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -7,7 +7,7 @@ class Solution:
 
         dp = [n for _ in range(n+1)]
         dp[0] = 0
-        dp[1] = 1
+       
         for i in range(1, n+1):
             for j in range(len(squre)):
                 if i-squre[j] < 0:
