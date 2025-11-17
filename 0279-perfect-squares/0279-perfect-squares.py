df539f10dee935b8ccb2275a152451f6124f2917
Gizachew Mohammed <gizachew980@gmail.com>
2025-11-18 01:09:41 +0300

    Time: 3023 ms (26.69%), Space: 18.1 MB (50.27%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        
+        squre = []
+        for i in range(ceil(sqrt(n))+1):
+            squre.append(i**2)
+
+        dp = [n for _ in range(n+1)]
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(1, n+1):
+            for j in range(len(squre)):
+                if i-squre[j] < 0:
+                    continue
+                dp[i] = min(dp[i], dp[i-squre[j]]+1)
+        print(dp)
+        return dp[-1]
