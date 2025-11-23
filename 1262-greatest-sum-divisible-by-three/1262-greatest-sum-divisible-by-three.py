c56fa0f80465502855907432606e964c3ece17dc
Gizachew Mohammed <gizachew980@gmail.com>
2025-11-24 02:58:08 +0300

    Time: 115 ms (27.86%), Space: 21.7 MB (66.57%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.py
@@ -1,23 +1,9 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        
-
-        ans = 0 
-
-        def dp(i, sums):
-            nonlocal ans
-
-            if i>=len(nums):
-                return
-
-            if (sums+nums[i])%3 == 0:
-                ans = max(ans, sums+nums[i])
-            
-            
-            for j in range(i+1, len(nums)):
-                dp(j, sums+nums[i])
-        
-        for k in range(len(nums)):
-            dp(k, 0)
-        
-        return ans
+        f = [0, -float("inf"), -float("inf")]
+        for num in nums:
+            g = f[:]
+            for i in range(3):
+                g[(i + num % 3) % 3] = max(g[(i + num % 3) % 3], f[i] + num)
+            f = g
+        return f[0]
