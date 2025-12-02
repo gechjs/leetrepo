a7fe5bc37eefc707aa76de5771ddf88e4b3cc01d
Gizachew Mohammed <gizachew980@gmail.com>
2025-12-02 08:46:45 +0300

    Time: 184 ms (5.03%), Space: 66.2 MB (15.04%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countTrapezoids(self, points: List[List[int]]) -> int:
+        
+        count = Counter()
+
+        for arr in points:
+            count[arr[1]]+=1
+        
+        ans = 0
+        point = 0
+        print(count)
+        for key, val in count.items():
+            if point==0:
+                n = val -1
+                point+= n*(n+1)//2
+            else:
+                n=val-1
+                ans+=point*(n*(n+1)//2)
+                point+=n*(n+1)//2
+        return ans%(10**9 +7)
