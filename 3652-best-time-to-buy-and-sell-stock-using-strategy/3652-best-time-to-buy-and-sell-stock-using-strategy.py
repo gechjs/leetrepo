9f8751bae7ff2f3f278ef70f91791af23c8896b9
Gizachew Mohammed <gizachew980@gmail.com>
2025-12-20 11:54:24 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.py
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.py
@@ -1,18 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
-        n = len(prices)
-        prefixPrice = [0] * (n + 1)
-        prefixSum = [0] * (n + 1)
-        for i in range(n):
-            prefixPrice[i] = prefixPrice[i-1] + prices[i]
-            prefixSum[i] = prefixSum[i-1] + strategy[i] * prices[i]
-        ans = prefixSum[-2]
-        for i in range(n+1-k):
-            start = i            
-            end = i + k - 1
-            mid = i + k // 2
-            tmp = prefixSum[-2] - (prefixSum[i+k-1] - prefixSum[i-1])
-            t = tmp + prefixPrice[end] - prefixPrice[mid-1]
-            ans = max(ans, t)
-        return ans
-            
+        
+
+        presum = []
+        for i in range(len(prices)):
+            presum.append(prices[i]*strategy[i])
+        
+        for i in range(1, len(presum)):
+            presum[i]+=presum[i-1]
+
+        for i in range(len(prices)):
+            prices[i]+=prices[i-1]
+        
+        maxgain = 0
+        gain = prices[k-1]-prices[k//2-1]-presum[k-1]
+        for i in range(k, len(prices)):
+            gain = prices[i-1]-prices[i//2-1]-presum[i-1]
+            maxgain = max(maxgain, gain)
+        return maxgain+presum[-1]
