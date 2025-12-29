81282a13cefca24ef543cebe6fb454d7334e7880
Gizachew Mohammed <gizachew980@gmail.com>
2025-12-30 01:54:09 +0300

    Time: 305 ms (10.36%), Space: 43.1 MB (5.24%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,26 +6,18 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]=="1":
-                    graph[(i, j)] = set()
-                    if i-1>=0:
-                        if grid[i-1][j] == "1":
-                           graph[(i, j)].add((i-1, j))
-                           graph[(i-1, j)].add((i, j))
                     if i+1<len(grid):
                         if grid[i+1][j]== "1":
                             graph[(i, j)].add((i+1, j))
                             graph[(i+1, j)].add((i, j))
-                        
-                    if j-1>=0:
-                        if grid[i][j-1] == "1":
-                            graph[(i, j)].add((i, j-1))
-                            graph[(i, j-1)].add((i, j))
 
                     if j+1<len(grid[0]):
                         if grid[i][j+1]== "1":
                             if grid[i][j+1]== "1":
                                 graph[(i, j)].add((i, j+1))
                                 graph[(i, j+1)].add((i, j))
+                    if not graph[(i, j)]:
+                        graph[(i, j)] = set()
         visited = set()
         ans = 0
         for node in graph:
