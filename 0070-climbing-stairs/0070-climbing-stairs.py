fa76cd9cc048ee90b499f5d6555138f1fe68b526
Gizachew Mohammed <gizachew980@gmail.com>
2026-01-02 19:11:31 +0300

    Time: 0 ms (100%), Space: 17.3 MB (89.4%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,11 +2,9 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         
         @cache
-        def dp(num):
-            if num == 0:
-                return 1
-            if num<0:
-                return 0
-        
-            return dp(num-1)+dp(num-2)
+        def dp(i):
+            if i<=2:
+                return i
+            
+            return dp(i-1)+dp(i-2)
         return dp(n)
